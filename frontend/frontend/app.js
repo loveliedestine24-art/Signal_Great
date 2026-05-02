fcb6c1f769fa5b243298c365b74e2fc02d27447e
loveliedestine24-art <loveliedestine24@gmail.com>
2026-05-02 04:56:15 -0400

    Update app.js

--- frontend/frontend/app.js
+++ frontend/frontend/app.js
@@ -1,6 +1,17 @@
-fetch("https://SEU-LINK.onrender.com/signal")
-.then(res => res.json())
-.then(data => {
-  document.getElementById("signal").innerText =
-    data.signal + " - " + data.score;
-});
+const API = "https://SEU-RENDER.onrender.com/market";
+
+async function loadData() {
+
+    const pair = document.getElementById("pair").value;
+    const tf = document.getElementById("tf").value;
+
+    const res = await fetch(`${API}?symbol=${pair}&interval=${tf}`);
+    const data = await res.json();
+
+    document.getElementById("signal").innerText = data.signal;
+    document.getElementById("price").innerText = "Price: " + data.price;
+    document.getElementById("score").innerText = "Score: " + data.score;
+    document.getElementById("trend").innerText = "Trend: " + data.trend;
+}
+
+setInterval(loadData, 5000);
