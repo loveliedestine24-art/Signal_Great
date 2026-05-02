f94bd8507e2c9097cdc16ccf7737b6a635c9f8a1
loveliedestine24-art <loveliedestine24@gmail.com>
2026-05-02 05:07:32 -0400

    Create candles.py

---
+++ backend/candles.py
@@ -0,0 +1,26 @@
+import requests
+
+def get_candles(symbol="BTCUSDT", interval="1m", limit=50):
+
+    url = "https://api.binance.com/api/v3/klines"
+
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "limit": limit
+    }
+
+    response = requests.get(url)
+    data = response.json()
+
+    candles = []
+
+    for c in data:
+        candles.append({
+            "open": float(c[1]),
+            "high": float(c[2]),
+            "low": float(c[3]),
+            "close": float(c[4]),
+        })
+
+    return candles
