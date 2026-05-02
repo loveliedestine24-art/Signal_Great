aadbf45beefd81b9ca4131309d9aa18cb14235b7
loveliedestine24-art <loveliedestine24@gmail.com>
2026-05-02 04:10:15 -0400

    Create candles.py

---
+++ backend/backend/candles.py
@@ -0,0 +1,30 @@
+import requests
+
+def get_candles(symbol="BTCUSDT", interval="1m", limit=50):
+    """
+    Pega candles reais da Binance (OHLC)
+    """
+
+    url = "https://api.binance.com/api/v3/klines"
+
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "limit": limit
+    }
+
+    response = requests.get(url)
+    data = response.json()
+
+    candles = []
+
+    for c in data:
+        candles.append({
+            "open": float(c[1]),
+            "high": float(c[2]),
+            "low": float(c[3]),
+            "close": float(c[4]),
+            "volume": float(c[5])
+        })
+
+    return candles
